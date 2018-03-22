3275efad0ae7955ad523e8007ae6db4539d38b3f
Basavaraju, Jagadish <jagadish.basavaraju@sap.com>
2018-03-22 12:39:13 +0530

    Fix for coverall integration

--- test/global.setup.js
+++ test/global.setup.js
@@ -5,6 +5,9 @@ const lib = require('../lib');
 const director = lib.bosh.director;
 let boshConfigCacheDetails = {};
 let deploymentIpsCacheDetails = {};
+
+process.on('unhandledRejection', (err) => console.log('Unhandled rejection - ', err.name));
+
 director.ready.then(() => {
   boshConfigCacheDetails = _.cloneDeep(director.boshConfigCache);
   deploymentIpsCacheDetails = _.cloneDeep(director.deploymentIpsCache);
@@ -13,4 +16,3 @@ beforeEach(function () {
   director.boshConfigCache = _.cloneDeep(boshConfigCacheDetails);
   director.deploymentIpsCache = _.cloneDeep(deploymentIpsCacheDetails);
 });
-
